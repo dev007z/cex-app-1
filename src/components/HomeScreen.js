5a812b3db16fd6629623258aea26bbde1ff0d02c
Junaid Bashir <junaid@crayondevelopers.com>
2023-08-12 20:17:57 +0100

    split screen style style buy/sell buttons

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../App.css";
 
 class HomeScreen extends Component {
     state = {
@@ -16,25 +17,21 @@ class HomeScreen extends Component {
             isBuySelected: false,
         });
     };
-
     render() {
-        const { isBuySelected } = this.state;
 
         return (
             <div className="home-screen">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
                 <div className="buy-sell-container">
-                    <button
-                        className={isBuySelected ? "active" : ""}
-                        onClick={this.handleBuyClick}
-                    >
-                        Buy
-          </button>
-                    <button
-                        className={!isBuySelected ? "active" : ""}
-                        onClick={this.handleSellClick}
-                    >
-                        Sell
-          </button>
+                    <div className="split top" onClick={this.handleBuyClick}>
+                        <h1 className="display-4 centered text-white">Buy</h1>
+                    </div>
+
+                    <div className="split bottom" onClick={this.handleSellClick}>
+                        <h1 className="display-4 centered text-black">Sell</h1>
+                    </div>
+                    
+                    
                 </div>
             </div>
         );
