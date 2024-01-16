289085142da256e0a8fc4cae9e31579c67c8ca8f
Junaid Bashir <junaid@crayondevelopers.com>
2024-01-16 20:27:32 +0100

    update naira rate

--- src/const.js
+++ src/const.js
@@ -7,11 +7,11 @@ const env = (name) => {
             serviceCharge: 0.05,
             serviceCharge1: 0.03,
             serviceCharge2: 0.025,
-            nairaRate: 1170
+            nairaRate: 1323
         } : {
             NODE_ENV,
             serviceCharge: 0.05,
-            nairaRate: 1170
+            nairaRate: 1323
         }
     )[name]
 }
